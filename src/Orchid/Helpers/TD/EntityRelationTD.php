30d592ab7009ca337149b889dc7692786ef06ed7
Andrey Manzadey <andrey.manzadey@gmail.com>
2023-08-01 16:38:15 +0300

    Update EntityRelationTD.php

--- src/Orchid/Helpers/TD/EntityRelationTD.php
+++ src/Orchid/Helpers/TD/EntityRelationTD.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Manzadey\LaravelOrchidHelpers\Orchid\Helpers\TD;
 
+use Illuminate\Database\Eloquent\Model;
 use Orchid\Screen\Layouts\Persona;
 use Orchid\Screen\TD;
 
@@ -14,9 +15,20 @@ class EntityRelationTD
         return TD::make($relation, $title)
             ->width('300px')
             ->render(
-                static fn($model) : ?Persona => data_get($model, $relation) ?
-                    new Persona(data_get($model, $relation)?->presenter()) :
-                    null
+                static function($model) use ($relation) : Persona|string {
+                    /* @var \Illuminate\Database\Eloquent\Model|null $model */
+                    $relationModel = data_get($model, $relation);
+
+                    if($relationModel instanceof Model === false) {
+                        return $model->getAttribute('subject_type');
+                    }
+
+                    if(method_exists($relationModel, 'presenter')) {
+                        return new Persona($relationModel->presenter());
+                    }
+
+                    return $relationModel->getMorphClass();
+                }
             );
     }
 }
